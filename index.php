b739e2db9cf2ae28726be767ce6e0a646bd4f973
imsaky <imsak.yanuar@students.amikom.ac.id>
2022-09-04 21:39:10 +0700

    iki nggo njajal

--- index.php
+++ index.php
@@ -9,35 +9,41 @@ $ambilDataBarang = mysqli_query($mysqli, "SELECT * FROM barang ORDER BY harga_ba
 <html>
 <head>    
     <title>Homepage</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
 </head>
  
 <body>
-<a href="add.php">Menambah Data Barang</a><br/><br/>
- 
-    <table width='80%' border=1>
-
-    <tr>
-        <th>Nama Barang Test</th>
-        <th>Jenis Barang</th> 
-        <th>Expired</th> 
-        <th>Harga Barang</th> 
-        <th>Jumlah Barang</th> 
-        <th>Aksi</th> 
     
-    </tr>
-    <?php  
-    
-    while($barang = mysqli_fetch_array($ambilDataBarang)) {         
-        echo "<tr>";
-        echo "<td>".$barang['nama_barang']."</td>";
-        echo "<td>".$barang['jenis_barang']."</td>";
-        echo "<td>".$barang['expired']."</td>";    
-        echo "<td>".$barang['harga_barang']."</td>";    
-        echo "<td>".$barang['jumlah_barang']."</td>";    
-        echo "<td><a href='edit.php?id=$barang[id]'>Edit</a> | <a href='delete.php?id=$barang[id]'>Delete</a></td></tr>";        
-    }
-    
-    ?>
-    </table>
+ <div class="container">
+    <a class="btn btn-primary mt-5 mb-5" href="add.php">Menambah Data Barang</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nama Barang Test</th>
+                    <th>Jenis Barang</th> 
+                    <th>Expired</th> 
+                    <th>Harga Barang</th> 
+                    <th>Jumlah Barang</th> 
+                    <th>Aksi</th> 
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($ambilDataBarang as $detailBarang ){ ?>
+                <tr>
+                    <td><?php echo $detailBarang['nama_barang'] ?></td>
+                    <td><?php echo $detailBarang['jenis_barang'] ?></td>
+                    <td><?php echo $detailBarang['expired'] ?></td>
+                    <td><?php echo $detailBarang['harga_barang'] ?></td>
+                    <td><?php echo $detailBarang['jumlah_barang'] ?></td>
+                    <td>
+                        <button class="btn btn-success" type="submit">Update</button>
+                        <button class="btn btn-danger" type="submit">Hapus</button>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+ </div>
 </body>
 </html>
